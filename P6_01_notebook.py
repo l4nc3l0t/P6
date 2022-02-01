20bcd289e0bfc2f54a9da354b38cfc8dbc231e99
lancelot <lancelot.leclercq@free.fr>
2022-02-01 17:12:57 +0100

    amélioration nettoyage

--- P6_01_notebook.py
+++ P6_01_notebook.py
@@ -12,6 +12,8 @@ data.info()
 # %%
 CategoryTree = data.product_category_tree.str.slice(
     start=2, stop=-2).str.split(' >> ', expand=True)
+for i in CategoryTree.columns:
+    CategoryTree.rename(columns={i: 'category_{}'.format(i)}, inplace=True)
 CategoryTree.head(3)
 # %%
 CategoryTree.info()
@@ -19,7 +21,7 @@ CategoryTree.info()
 # Les 3 premières hiérarchies de catégories sont bien remplies les suivantes à moitié
 
 # %% [markdown]
-# nettoyage des spécifications produits et mise sous forme de dictionnaire
+# nettoyage des spécifications produits et mise sous forme de dataframe
 # %%
 # nettoyage
 Specifications = data.product_specifications.str.replace(
@@ -56,7 +58,7 @@ SpecificationsClean = SpecificationsClean.str.replace('{nil}',
                                                           regex=True)
 SpecificationsClean.fillna('{"unknown":"unknown"}', inplace=True)
 # %%
-# mise sous forme de dictionnaire
+# mise sous forme de dictionnaire puis de dataframe
 for i in SpecificationsClean.index:
     spec = ast.literal_eval(SpecificationsClean[i])
     if i == 0:
@@ -73,11 +75,21 @@ for i in SpecificationsClean.index:
 # %%
 ProdSpec.head()
 # %%
-# sélection des colonnes comportant le plus de données
+# visualisation des colonnes comportant le plus de données
 ProdSpec.isna().sum().sort_values().head(15)
 # %%
+# conservation des 7 colonnes contenant le plus de données
 ProdSpecClean = ProdSpec[ProdSpec.isna().sum().sort_values().head(
-    12).index.drop('unknown').to_list()]
+    8).index.drop('unknown').to_list()]
 ProdSpecCleanFill = ProdSpecClean.fillna('unknown')
 ProdSpecCleanFill.head()
+# %%
+# utilisation des 3 premières branches de catégories
+TextData = CategoryTree.iloc[:, :3].merge(
+    ProdSpecCleanFill, left_index=True,
+    right_index=True).merge(data[['pid', 'description']], left_index=True,
+                            right_index=True).set_index('pid').reset_index()
+# %%
+# liste des identifiants produits et des fichiers d'image associés
+ImgList = data[['pid', 'image']]
 # %%
