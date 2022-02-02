2eb4ea422b1a61e1ccd7706809b4c00975f80335
lancelot <lancelot.leclercq@free.fr>
2022-02-02 20:35:35 +0100

    add création fichiers, améliorations fig freq

--- P6_01_notebook.py
+++ P6_01_notebook.py
@@ -1,4 +1,5 @@
 # %%
+import os
 import pandas as pd
 import numpy as np
 import ast
@@ -8,6 +9,28 @@ import nltk
 
 nltk.download(['punkt', 'stopwords', 'wordnet', 'omw-1.4'],
               '.env/lib/nltk_data')
+
+# %%
+write_data = True
+
+# True : création d'un dossier Figures et Tableau
+# dans lesquels seront créés les éléments qui serviront à la présentation
+# et écriture des figures et tableaux dans ces dossier
+#
+# False : pas de création de dossier ni de figures ni de tableaux
+
+if write_data is True:
+    try:
+        os.mkdir("./Figures/")
+    except OSError as error:
+        print(error)
+    try:
+        os.mkdir("./Tableaux/")
+    except OSError as error:
+        print(error)
+else:
+    print("""Visualisation uniquement dans le notebook
+    pas de création de figures ni de tableaux""")
 
 # %%
 data = pd.read_csv('./flipkart_com-ecommerce_sample_1050.csv')
@@ -91,7 +114,7 @@ ProdSpecClean.head()
 # %%
 # utilisation des 3 premières branches de catégories
 TextData = CategoryTree.iloc[:, :3].merge(
-    ProdSpecCleanFill, left_index=True,
+    ProdSpecClean, left_index=True,
     right_index=True).merge(data[['pid', 'description']],
                             left_index=True,
                             right_index=True).set_index('pid').reset_index()
@@ -100,7 +123,7 @@ TextData = CategoryTree.iloc[:, :3].merge(
 ImgList = data[['pid', 'image']]
 
 
-# %%
+# %%
 # nettoyage ponctuation, nombres
 def clean_text(text):
     textNpunct = ''.join(
@@ -117,7 +140,7 @@ Tokens = {}
 for r in range(len(TextData)):
     Tokens[TextData.pid[r]] = nltk.word_tokenize(Descriptions.loc[r].lower())
 # %%
-# nettoyage stopwords
+# nettoyage stopwords
 stopW = nltk.corpus.stopwords.words('english')
 stopW.append([char for char in string.ascii_lowercase])  # supp lettres uniques
 TokensClean = {}
@@ -128,13 +151,15 @@ for r in range(len(TextData)):
     ]
 
 # %%
-# lemmatisation
+# lemmatisation
 Lems = {}
 for r in range(len(TextData)):
     Lems[TextData.pid[r]] = [
         nltk.WordNetLemmatizer().lemmatize(word)
         for word in TokensClean[TextData.pid[r]]
     ]
+# %% [markdown]
+# Visualisation des mots ayant le plus d'occurences dans tout le jeux de données
 # %%
 FullTok = []
 for r in range(len(TextData)):
@@ -145,13 +170,18 @@ FreqFull = pd.DataFrame({
     'Mot': nltk.FreqDist(FullTok).keys(),
     'Freq': nltk.FreqDist(FullTok).values()
 })
-FreqFull['Freq_%'] = round(FreqFull.Freq * 100 / FreqFull.Freq.count(), 2)
+FreqFull['Freq_%'] = round(FreqFull.Freq * 100 / FreqFull.Freq.sum(), 2)
 FreqFull.sort_values(['Freq'], ascending=False).head(20)
 
 # %%
+# 50 premiers mots
 fig = px.bar(FreqFull.sort_values(['Freq'], ascending=False).head(50),
              x='Mot',
              y='Freq',
-             width=1000)
+             width=900,
+             labels={'Freq':"Nb d'occurences", 'Mot':'Mots'})
 fig.show(renderer='notebook')
+if write_data is True:
+    fig.write_image('./Figures/FreqTok50.pdf')
+    fig.write_image('./Figures/FreqTok50.pdf')
 # %%
