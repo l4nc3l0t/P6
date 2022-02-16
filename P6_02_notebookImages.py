3f839a07c8a9f3b45dab27a86e3823543296997b
lancelot <lancelot.leclercq@free.fr>
2022-02-16 13:09:51 +0100

    visu traitements img 2.0, création imgProc

--- P6_02_notebookImages.py
+++ P6_02_notebookImages.py
@@ -65,6 +65,8 @@ DataImages = data[['pid', 'image']].join(CategoryTree.category_0)
 # Nb d'images par cat
 DataImages.groupby('category_0').agg({'image': 'count'})
 # %%
+DataImages['path'] = ['./Images/' + name for name in DataImages.image]
+# %%
 # test ouverture image
 fig = px.imshow(cv.imread(DataImages.path[0]))
 fig.show(renderer='notebook')
@@ -81,53 +83,123 @@ fig.update_layout(coloraxis_showscale=False)
 fig.update_xaxes(showticklabels=False)
 fig.update_yaxes(showticklabels=False)
 fig.show(renderer='jpeg')
+
+
 # %%
 def figVisuImg(img):
     fig = make_subplots(
         1,
         3,
-        subplot_titles=('Image',
-                        'Histogramme des pixels<br>par niveaux de gris',
-                        'Histogramme des pixels<br>par niveaux de gris cumulés'),
+        subplot_titles=(
+            'Image', 'Histogramme des pixels<br>par niveaux de gris',
+            'Histogramme des pixels<br>par niveaux de gris cumulés'),
         horizontal_spacing=.12)
     fig.add_trace(
-        go.Heatmap(z=imgG, colorscale='gray', showscale=False, name='Image'))
+        go.Heatmap(z=img, colorscale='gray', showscale=False, name='Image'))
     fig.update_xaxes(showticklabels=False, row=1, col=1)
     fig.update_yaxes(showticklabels=False, autorange='reversed', row=1, col=1)
     fig.add_trace(
         go.Histogram(x=img.flatten(),
-                    ybins={'size': 1},
-                    name='NivGris',
-                    showlegend=False), 1, 2)
+                     ybins={'size': 1},
+                     name='NivGris',
+                     showlegend=False), 1, 2)
     fig.add_trace(
         go.Histogram(x=img.flatten(),
-                    ybins={'size': 1},
-                    cumulative_enabled=True,
-                    name='CumNivGris',
-                    showlegend=False), 1, 3)
+                     ybins={'size': 1},
+                     cumulative_enabled=True,
+                     name='CumNivGris',
+                     showlegend=False), 1, 3)
     fig.update_yaxes(title_text='Nombre de pixels', row=1, col=2)
     fig.update_yaxes(title_text='Nombre de pixels cumulés', row=1, col=3)
     fig.update_xaxes(title_text='Niveaux de gris', row=1, col=2)
     fig.update_xaxes(title_text='Niveaux de gris', row=1, col=3)
     fig.update_layout(height=350, width=800)
     return fig
-# %%
-imgG = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
-fig = figVisuImg(imgG)
-fig.show(renderer='jpeg')
-if write_data is True:
-    fig.write_image('./Figures/imgGrey.pdf')
+
 
 # %%
-imgGFilt = cv.GaussianBlur(imgG, (3, 3), cv.BORDER_DEFAULT)
-fig = figVisuImg(imgGFilt)
+imgBW = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
+fig = figVisuImg(imgBW)
+fig.update_layout(title_text='Image convertie en niveaux de gris')
 fig.show(renderer='jpeg')
 if write_data is True:
-    fig.write_image('./Figures/imgGFilt.pdf')
+    fig.write_image('./Figures/imgBW.pdf')
 # %%
-imgGFiltEQ = cv.equalizeHist(imgGFilt)
-fig = figVisuImg(imgGFiltEQ)
+# égalisation par equalizeHist
+imgBWEQ = cv.equalizeHist(imgBW)
+fig = figVisuImg(imgBWEQ)
+fig.update_layout(title_text='Image égalisée par equalizeHist')
 fig.show(renderer='jpeg')
 if write_data is True:
-    fig.write_image('./Figures/imgGFiltEQ.pdf')
+    fig.write_image('./Figures/imgBWEQ.pdf')
+# %%
+# filtration GaussianBlur
+imgBWEQGaussFilt = cv.GaussianBlur(imgBWEQ, (3, 3), cv.BORDER_DEFAULT)
+fig = figVisuImg(imgBWEQGaussFilt)
+fig.update_layout(
+    title_text='Image égalisée par equalizeHist et filtrée par GaussianBlur')
+fig.show(renderer='jpeg')
+if write_data is True:
+    fig.write_image('./Figures/imgBWEQGaussFilt.pdf')
+
+# %%
+# filtration Non-local Means Denoising
+imgBWEQNlMDFilt = cv.fastNlMeansDenoising(imgBWEQ, None, 5, 7, 21)
+fig = figVisuImg(imgBWEQNlMDFilt)
+fig.update_layout(
+    title_text=
+    'Image égalisée par equalizeHist et filtrée par Non-local Means Denoising')
+fig.show(renderer='jpeg')
+if write_data is True:
+    fig.write_image('./Figures/imgBWEQNlMDFilt.pdf')
+
+# %%
+# égalisation par CLAHE
+imgBWCLAHE = cv.createCLAHE(clipLimit=8, tileGridSize=(3, 3)).apply(imgBW)
+fig = figVisuImg(imgBWCLAHE)
+fig.update_layout(title_text='Image égalisée par CLAHE')
+fig.show(renderer='jpeg')
+if write_data is True:
+    fig.write_image('./Figures/imgBWCLAHE.pdf')
+# %%
+# filtration GaussianBlur
+imgBWCLAHEGaussFilt = cv.GaussianBlur(imgBWCLAHE, (3, 3), cv.BORDER_DEFAULT)
+fig = figVisuImg(imgBWCLAHEGaussFilt)
+fig.update_layout(
+    title_text='Image égalisée par CLAHE et filtrée par GaussianBlur')
+fig.show(renderer='jpeg')
+if write_data is True:
+    fig.write_image('./Figures/imgBWCLAHEGaussFilt.pdf')
+# %%
+# filtration NlMD
+imgBWCLAHENlMDFilt = cv.fastNlMeansDenoising(imgBWCLAHE, None, 5, 7, 21)
+fig = figVisuImg(imgBWCLAHENlMDFilt)
+fig.update_layout(
+    title_text=
+    'Image égalisée par CLAHE et filtrée par Non-local Means Denoising')
+fig.show(renderer='jpeg')
+if write_data is True:
+    fig.write_image('./Figures/imgBWCLAHENlMDFilt.pdf')
+# %% [markdown]
+# La combinaison de l'égalisation par CLAHE et de la filtration par NlMD semble
+# nous retourner une image bien équilibrée (courbe cumulée régulière).
+# Nous utiliserons ces traitements pour l'ensemble des images
+# %% [markdown]
+#### Création des images traitées
+# %%
+if write_data is True:
+    try:
+        os.mkdir("./ImagesProcessed/")
+    except OSError as error:
+        print(error)
+    for path, name in zip(DataImages.path, DataImages.image):
+        img = cv.imread(path)
+        imgR = cv.resize(img, (224, 224), interpolation=cv.INTER_AREA)
+        imgBW = cv.cvtColor(imgR, cv.COLOR_BGR2GRAY)
+        imgBWCLAHE = cv.createCLAHE(clipLimit=8,
+                                    tileGridSize=(3, 3)).apply(imgBW)
+        imgBWCLAHENlMD = cv.fastNlMeansDenoising(imgBWCLAHE, None, 5, 7, 21)
+        cv.imwrite('./ImagesProcessed/' + name, imgBWCLAHENlMD)
+else:
+    print("""Autoriser l'écriture des fichier en changeant write_data=True""")
 # %%
